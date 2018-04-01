d66a1fce3e042792eb32bf0f39ab0b9bc85a76f0
LittleSheep520 <LittleSheep520@outlook.com>
2018-04-01 13:17:23 +0800

    lachange od wayay

--- main.js
+++ main.js
@@ -5,14 +5,24 @@ $(window).load(function(){
         });
     }
 });
-
+function showJdPage(){
+    console.log(getPage());
+    self.parent.frames["mainFrame"].document.body.innerHTML = "<div src=''></div>"
+}
+function showTbPage(){
+    console.log(getPage());
+    self.parent.frames["mainFrame"].document.body.innerHTML = "<h1>淘宝订单数据同步管理页</h1>";
+}
 $(document).ready(function(){
+    console.log(getPage());
     if (getPage() == "topbar.aspx"){
         //page
-        $("<div id='jd-menu' width='420' class='nav'><iframe width='420' align='center' scrolling='no' height='60' frameborder='0' allowtransparency='true'></iframe></div>").appendTo("#main")
+        $("<div id='jd-menu' class='nav' onclick=showJdPage()><div></div>京东订单</div>").appendTo("#main")
+        $("<div id='tb-menu' class='nav' onclick=showTbPage()><div></div>淘宝订单</div>").appendTo("#main")
         $("<div class='nav'id='tianqi'><iframe width='420' align='center' scrolling='no' height='60' frameborder='0' allowtransparency='true' src='http://i.tianqi.com/index.php?c=code&id=12&icon=1&num=3'></iframe></div>").appendTo("#main")
         //css
-        $("#jd-menu").css({"background-image": "url(https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd_menu_button.png)"});
+        $("#jd-menu").css({"background-image": "url(https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd_menu_btn.png)","background-repeat":"no-repeat","background-position":"center top"});
+        $("#tb-menu").css({"background-image": "url(https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/tb_menu_btn.png)","background-repeat":"no-repeat","background-position":"center top"});
         //js
     }
 })
