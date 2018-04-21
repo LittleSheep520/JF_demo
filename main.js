31f1426f5208b6c38060775ae544b51fe5ab1543
LittleSheep520 <LittleSheep520@outlook.com>
2018-04-21 15:15:37 +0800

    +

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 <script type="text/javascript">
     function showJdPage(){
         self.parent.frames["mainFrame"].document.body.innerHTML ="<div id='mainDiv'  style='overflow:scroll'></div>"
-        //$("#mainDiv",self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd.html");
-        $("#mainPluginJs-menu",self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd.html");
+        $("#mainDiv",self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd.html");
+        //$("#mainPluginJs-menu",self.parent.frames["mainFrame"].document).load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd.html");
     }
     function showTbPage(){
         self.parent.frames["mainFrame"].document.body.innerHTML = "<h1>淘宝订单数据同步管理页</h1>";
     }
     $(document).ready(function(){
+            $("#mainPluginJs-menu").load("https://raw.githubusercontent.com/LittleSheep520/JF_demo/master/jd.html");
             //page
             $("<div class='sep'></div><div id='jd-menu' class='nav' onclick=showJdPage()><div></div>京东订单</div><div class='sep'></div>").appendTo("#main")
             $("<div id='tb-menu' class='nav' onclick=showTbPage()><div></div>淘宝订单</div>").appendTo("#main")
